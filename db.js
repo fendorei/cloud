73209554f6fb761f2ad75657f10cf87b556e35d9
fendorei <antoine.luta@gmail.com>
2020-12-17 16:54:38 +0100

    influencer

---
+++ db.js
@@ -0,0 +1,23 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'my_user',
+  host: 'localhost',
+  database: 'my_database',
+  password: '1948',
+  port: 5432,
+});
+
+const getInfluencer = () => {
+    return new Promise(function(resolve, reject) {
+      pool.query('SELECT * FROM influenceurs ORDER BY id ASC', (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(results.rows);
+      })
+    }) 
+  }
+
+module.exports = {
+  getInfluencer
+}
--- merchant_model.js
+++
@@ -1,8 +0,0 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'my_user',
-  host: 'localhost',
-  database: 'my_database',
-  password: '1948',
-  port: 5432,
-});
